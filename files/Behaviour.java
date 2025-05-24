1ae83aa80a86fbd54b12f02f0a2ecaf35b927d76
joaoC6 <1956bec.orelha@gmail.com>
2025-05-24 18:24:01 +0100

    a

---
+++ files/Behaviour.java
@@ -0,0 +1,12 @@
+package files;
+
+
+/**
+ * Class responsible for receiving the input of the user and transmiting to the other classes
+ * @author poo2425p1g4
+ * @version 1.0
+ * **/
+
+public class Behaviour {
+
+}
