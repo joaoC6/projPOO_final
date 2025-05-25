cf78cb4720d7166e30c73fb6fe65705b9b605b4e
Tiago-Santos594 <a83874@ualg.pt>
2025-05-25 17:57:10 +0100

    Add files via upload

---
+++ src/files/Circulo.java
@@ -0,0 +1,106 @@
+package files;
+
+/**
+ * Representa um círculo no plano cartesiano.
+ * @author POO2425p1g4
+ * @version 01/04/2025
+ * @inv O raio deve ser maior que 0, o centro deve estar no primeiro quadrante
+ */
+public class Circulo {
+    private Ponto centro;
+    private double raio;
+
+    /**
+     * Constrói um círculo a partir de uma string que contém as coordenadas do centro e o raio.
+     * Caso os parâmetros sejam inválidos (ex.: raio <= 0 ou centro fora do primeiro quadrante),
+     * o programa imprime "Circulo:vi" e finaliza a execução.
+     * @param params String no formato "cx cy r".
+     */
+    public Circulo(String params) {
+        //super(params);
+        String[] tokens = params.trim().split("\\s+");
+        if (tokens.length != 3) {
+            System.out.println("Circulo:vi");
+            System.exit(0);
+        }
+        int cx = Integer.parseInt(tokens[0]);
+        int cy = Integer.parseInt(tokens[1]);
+        double r = Double.parseDouble(tokens[2]);
+        if (validaCirculo(cx, cy, r)) {
+            System.out.println("Circulo:vi");
+            System.exit(0);
+        }
+        centro = new Ponto(cx, cy);
+        raio = r;
+    }
+
+    /**
+     * Constrói um círculo a partir de uma string que contém as coordenadas do centro e o raio.
+     * Caso os parâmetros sejam inválidos (ex.: raio <= 0 ou centro fora do primeiro quadrante),
+     * o programa imprime "Circulo:vi" e finaliza a execução.
+     * @param centro center point of the circle.
+     * @param raio the radius of the circle.
+     */
+    public Circulo(Ponto centro, double raio) {
+        //super(null);
+        double cx = centro.getX();
+        double cy = centro.getY();
+        this.centro = new Ponto(cx, cy);
+        this.raio = raio;
+    }
+
+    /**
+     * Verifica se as coordenadas do centro e o raio definem um círculo válido.
+     * @param cx Coordenada x do centro.
+     * @param cy Coordenada y do centro.
+     * @param r  Raio do círculo.
+     * @return true se for inválido, false caso contrário.
+     */
+    private static boolean validaCirculo(double cx, double cy, double r) {
+        return (cx < 0 || cy < 0 || r <= 0);
+    }
+
+    /**
+     * Aplica a translação ao círculo, movendo o centro.
+     * @param dx deslocamento em x.
+     * @param dy deslocamento em y.
+     */
+    public void translacao(double dx, double dy) {
+        Ponto novoCentro = centro.translacaoPonto(dx, dy);
+        if (validaCirculo((int) novoCentro.getX(), (int) novoCentro.getY(), this.raio)) {
+            System.out.println("Circulo:vi");
+            System.exit(0);
+        }
+        this.centro = novoCentro;
+    }
+
+    /**
+     * Retorna o raio do círculo.
+     * @return O valor do raio.
+     */
+    public double getRaio() {
+        return raio;
+    }
+    /**
+     * Retorna o centro do círculo.
+     * @return Um objeto Ponto representando o centro.
+     */
+    public Ponto getCentro() {
+        return centro;
+    }
+
+    /**
+     * muda o valor do raio do circulo
+     */
+    public void setRaio(double raio) { this.raio = raio;}
+
+    /**
+     * Retorna uma representação textual do círculo, incluindo
+     * o centro e o raio em formato "Circulo: (cx,cy) r".
+     * @return A string que descreve o círculo.
+     */
+    @Override
+    public String toString() {
+        return String.format("%s %f", centro, raio);
+    }
+}
---
+++ src/files/GUI.java
@@ -0,0 +1,62 @@
+package files;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Class responsible for the grafical display of the game engine
+ * @author poo2425p1g4
+ * @version 1.0
+ * */
+
+public class GUI extends JPanel {
+    private final BufferedImage backGround;
+    private CopyOnWriteArrayList<GameObject> enabledObjects;
+
+    public GUI() {
+        enabledObjects = new CopyOnWriteArrayList<>();
+        try{
+            File f = new File("src/resources/Images/background.png");
+            backGround = ImageIO.read(f);
+//            backGround = ImageIO.read(getClass().getResource("/background.png"));
+        }catch(IOException e) {throw new RuntimeException("Nao encontrou o ficheiro");}
+
+        setBackground(Color.BLACK);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.drawImage(backGround, 0, 0, backGround.getWidth(), backGround.getHeight(), null);
+        for(GameObject go : enabledObjects){
+            Shape s = go.shape();
+            Ponto p = go.transform().position();
+            if(go.name().compareTo("parede") != 0)g2.drawImage(s.img(), (int) p.getX(), (int) p.getY(),(int) (Main.windowWidth * 0.1), (int) (Main.windowHeigth * 0.1), null);
+            else g2.drawImage(s.img(), (int) p.getX(), (int) p.getY(), 20, Main.windowHeigth*2, null);
+        }
+
+        g2.setFont(new Font("Times New Roman", Font.LAYOUT_LEFT_TO_RIGHT, 20));
+        g2.setColor(Color.white);
+        g2.drawString("Score:", 10, 20);
+
+
+    }
+
+    public void render(ArrayList<GameObject> enabled){
+//        JFrame jf = new JFrame();
+//        jf.setSize(Main.windowWidth, Main.windowHeigth);
+        enabledObjects.clear();
+        enabledObjects.addAll(enabled);
+        repaint();
+//        jf.add(this);
+//        jf.setVisible(true);
+    }
+}
